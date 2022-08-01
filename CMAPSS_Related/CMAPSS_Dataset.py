3dfb75458706960b90e26fb3e94706dee73ecdb5
Junkun-Lu <68013664+Junkun-Lu@users.noreply.github.com>
2022-08-01 11:08:35 +0200

    Dataset of CMAPSS

---
+++ CMAPSS_Related/CMAPSS_Dataset.py
@@ -0,0 +1,22 @@
+from torch.utils.data import Dataset
+
+"""
+In this file, the three datasets will be processed by the Dataset; 
+The output will be used as input to the Experiment_CMaps(gai).py.
+"""
+
+class CMAPSSData(Dataset):
+
+    def __init__(self, data_x, data_y):
+        self.data_x = data_x
+        self.data_y = data_y
+        self.data_channel = data_x.shape[2]
+
+    def __getitem__(self, index):
+        sample_x = self.data_x[index]
+        sample_y = self.data_y[index]
+
+        return sample_x, sample_y
+
+    def __len__(self):
+        return len(self.data_x)

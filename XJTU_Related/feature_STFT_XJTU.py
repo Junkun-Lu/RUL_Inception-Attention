4e58487306a85839f9dfcc8a59aeba1c6de1e1a8
Junkun-Lu <68013664+Junkun-Lu@users.noreply.github.com>
2022-08-09 18:27:07 +0200

    extract feature by STFT

---
+++ XJTU_Related/feature_STFT_XJTU.py
@@ -0,0 +1,98 @@
+import numpy as np
+from scipy import signal
+import matplotlib.pyplot as plt
+
+# 绘制和STFT相关的合力的vibration图片
+def vibration_train_picture(train_x_dataframe, train_file_num_ls, train_bearing_data_set): 
+    plt.figure(figsize=(12, 3*len(train_file_num_ls)))  
+    for i in range(0, len(train_file_num_ls)):
+        # 每一个file包含32768个文件
+        temp_split_dataframe = train_x_dataframe.iloc[:32768*train_file_num_ls[i], -1]
+        train_x_dataframe = train_x_dataframe.iloc[32768*train_file_num_ls[i]:, -1]
+        train_x_dataframe = train_x_dataframe.reset_index()
+        
+        nums = i + 1
+        plt.subplot(len(train_file_num_ls), 1, nums)
+        plt.plot(temp_split_dataframe)
+        plt.title("Vibration of {}".format(train_bearing_data_set[i]))
+        plt.xlabel("sample file number")
+        plt.ylabel("vibration signal") 
+    plt.tight_layout()
+    plt.savefig('/content/drive/MyDrive/picture_XJTU/vibration_picture/train_vibration_picture/vibration_of_training_set_{}.jpg'.format(train_bearing_data_set[0].split("_")[0]))  
+
+def vibration_test_picture(test_x_dataframe, test_bearing_data_set):
+    plt.figure(figsize=(12, 3))
+    plt.plot(test_x_dataframe[['vibration']])
+    plt.title("Vibration of {}".format(test_bearing_data_set[0]))
+    plt.xlabel("sample file number")
+    plt.ylabel("vibration signal")
+    plt.tight_layout()     
+    plt.savefig('/content/drive/MyDrive/picture_XJTU/vibration_picture/test_vibration_picture/vibration_of_test_set_{}.jpg'.format(test_bearing_data_set[0])) 
+
+
+# STFT data calculate:   25.6*10e3
+def STFT_process(vibration, STFT_window_len, STFT_overlap_num):
+    fs = 25.6*10e3   # sample frequence
+    f, t, zxx = signal.stft(vibration, fs, window="hann", nperseg=STFT_window_len, noverlap=STFT_overlap_num)
+    zxx = abs(zxx)    # remove imaginary type
+    zxx = np.expand_dims(zxx, axis=0)
+    return zxx, f, t  
+
+
+# 绘制每个Bearing第一个和最后一个文件的STFT图
+def STFT_picture_train(train_stft_feature, train_stft_f, train_stft_t, train_file_num_ls, train_bearing_data_set):
+    plt.figure(figsize=(12, 3*len(train_file_num_ls)))
+    for i in range(0, len(train_file_num_ls)):
+        split_train_stft_feature = train_stft_feature[0:train_file_num_ls[i], ::]
+        train_stft_feature = train_stft_feature[train_file_num_ls[i]:, ::]
+
+        # plot1 - STFT of bearingx_i in first file
+        plt.subplot(len(train_file_num_ls), 2, int(2*i+1))
+        plt.pcolormesh(train_stft_t, train_stft_f, split_train_stft_feature[0, ::], shading='gouraud')
+        plt.title("STFT of {} in first file".format(train_bearing_data_set[i]))
+        plt.xlabel("time(s)")
+        plt.ylabel("Frequency(Hz)")  
+
+        # plot2 - STFT of bearingx_i in last file
+        plt.subplot(len(train_file_num_ls), 2, int(2*i+2))  
+        plt.pcolormesh(train_stft_t, train_stft_f, split_train_stft_feature[-1, ::], shading='gouraud')
+        plt.title("STFT of {} in first file".format(train_bearing_data_set[i]))
+        plt.xlabel("time(s)")
+        plt.ylabel("Frequency(Hz)")
+    plt.tight_layout()
+    plt.savefig('/content/drive/MyDrive/picture_XJTU/STFT_picture/train_STFT_picture/STFT_of_training_set_{}.jpg'.format(train_bearing_data_set[0].split("_")[0]))      
+
+def STFT_picture_test(test_stft_feature, test_stft_f, test_stft_t, test_bearing_data_set):
+    plt.figure(figsize=(12, 3))
+    # plot1 - STFT of Bearingx_j in first file
+    plt.subplot(121)   
+    plt.pcolormesh(test_stft_t, test_stft_f, test_stft_feature[0,::], shading='gouraud')
+    plt.title("STFT of {} in first file".format(test_bearing_data_set[0]))
+    plt.xlabel("time(s)")
+    plt.ylabel("Frequency(Hz)")
+
+    # plot2 - STFT of Bearingx_j in last file
+    plt.subplot(122)   
+    plt.pcolormesh(test_stft_t, test_stft_f, test_stft_feature[-1,::], shading='gouraud')
+    plt.title("STFT of {} in last file".format(test_bearing_data_set[0]))
+    plt.xlabel("time(s)")
+    plt.ylabel("Frequency(Hz)")
+
+    plt.tight_layout()
+    plt.savefig('/content/drive/MyDrive/picture_XJTU/STFT_picture/test_STFT_picture/STFT_test_{}.jpg'.format(test_bearing_data_set[0])) 
+
+
+# STFT的移动滑窗
+def STFT_sliding_window(stand_train_stft_feature, y_array, window_length):
+    seg_x_ls = []
+    seg_y_ls = []
+    for i in range(0, len(y_array)-window_length+1):
+        seg_x = stand_train_stft_feature[i:i+window_length, ::]
+        seg_x = np.expand_dims(seg_x, axis=0)
+        seg_x_ls.append(seg_x)
+        seg_y = y_array[i:i+window_length, :]
+        seg_y = seg_y.transpose(1,0)
+        seg_y_ls.append(seg_y)
+    seg_x_np = np.concatenate(seg_x_ls, axis=0)
+    seg_y_np = np.concatenate(seg_y_ls, axis=0)
+    return seg_x_np, seg_y_np
